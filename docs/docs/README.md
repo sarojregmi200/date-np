4372ae23b999d20386d326311a85902c80757aed
sarojregmi200 <sarojregmi.official@gmail.com>
2024-12-18 12:20:45 +0545

    feat(docs): auto gen docs on change done

--- docs/docs/README.md
+++ docs/docs/README.md
@@ -3,3 +3,5 @@
 ***
 
 # date-np
+
+Date-np is a simple & minimal Nepali date picker that just works.
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-
-
+/**
+ * Date-np is a simple & minimal Nepali date picker that just works.
+ * @packageDocumentation
+ */
