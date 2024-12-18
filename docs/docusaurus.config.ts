77ee1a5c726743d669d517b3d72711dfc17e702e
sarojregmi200 <sarojregmi.official@gmail.com>
2024-12-18 10:42:00 +0545

    chore: docusaurus initialized

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,96 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+    title: 'Date-np',
+    tagline: 'Simple & minimal Nepali date picker that just works. ',
+    favicon: 'img/favicon.ico',
+
+    // Set the production url of your site here
+    url: 'https://bright-office.github.io',
+    baseUrl: '/react-hooks/',
+    trailingSlash: true,
+
+    // GitHub pages deployment config.
+    organizationName: 'bright-office',
+    projectName: 'react-hooks',
+
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
+
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    sidebarPath: './sidebars.ts',
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/sarojregmi200/date-np',
+                },
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            } satisfies Preset.Options,
+        ],
+    ],
+
+    themeConfig: {
+        navbar: {
+            title: 'Date-np',
+            /* logo: {
+                alt: 'My Site Logo',
+                src: 'img/logo.svg',
+            }, */
+            items: [
+                {
+                    type: 'docSidebar',
+                    sidebarId: 'docsSidebar',
+                    position: 'left',
+                    label: 'Docs',
+                },
+                {
+                    href: 'https://github.com/sarojregmi200/date-np',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
+        },
+
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Social',
+                    items: [
+                        {
+                            label: 'Source',
+                            to: "https://github.com/sarojregmi200/date-np",
+                        },
+                        {
+                            label: 'Bright',
+                            to: "https://github.com/bright-office",
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} bright-office and the maintainers. Built with Docusaurus.`,
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+    } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ docs/src/pages/index.tsx
@@ -0,0 +1,41 @@
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import Heading from '@theme/Heading';
+
+import styles from './index.module.css';
+import { JSX } from 'react';
+
+function HomepageHeader() {
+    const { siteConfig } = useDocusaurusContext();
+
+    return (
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+            <div className="container">
+                <Heading as="h1" className="hero__title">
+                    {siteConfig.title}
+                </Heading>
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+                <div className={styles.buttons}>
+                    <Link
+                        className="button button--secondary button--lg"
+                        to="/docs/intro">
+                        Get Started
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default function Home(): JSX.Element {
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <Layout
+            title={`${siteConfig.title}`}
+            description="Date-np is a simple & minimal Nepali date picker that just works.">
+            <HomepageHeader />
+        </Layout>
+    );
+}
