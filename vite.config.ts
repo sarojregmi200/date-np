c7265c6d57060c362a5ce0c75609964487a9576d
Saroj <sarojregmi.official@gmail.com>
2025-01-05 20:07:10 +0545

    chore: using `minify:false` in dev preview for easier debugging.

--- vite.config.ts
+++ vite.config.ts
@@ -4,38 +4,42 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from "vite-plugin-dts";
 
-// https://vite.dev/config/
-export default defineConfig({
-    build: {
-        lib: {
-            entry: resolve(__dirname, "src/index.tsx"),
-            name: "date-np",
-            formats: ["es"],
-        },
-        outDir: "dist",
-        emptyOutDir: true,
-        rollupOptions: {
-            // Means;
-            // don't include this in the final build.
-            // these are required to be present in the env where this is going to be used. 
-            external: ['react', 'react-dom', 'react/jsx-runtime'],
-            output: {
-                intro: (chunk) => {
-                    if (chunk.fileName.includes('src/index.js')) {
-                        return `import "../style.css";`
-                    }
-                    return ''
-                },
-                preserveModules: true,
-                dir: 'dist',
-                entryFileNames: '[name].js',
-                exports: 'named'
+export default defineConfig(({ mode }) => {
+    const isReleaseBuild = mode === "release";
+
+    return {
+        build: {
+            minify: isReleaseBuild,
+            lib: {
+                entry: resolve(__dirname, "src/index.tsx"),
+                name: "date-np",
+                formats: ["es"],
+            },
+            outDir: "dist",
+            emptyOutDir: true,
+            rollupOptions: {
+                // Means;
+                // don't include this in the final build.
+                // these are required to be present in the env where this is going to be used. 
+                external: ['react', 'react-dom', 'react/jsx-runtime'],
+                output: {
+                    intro: (chunk) => {
+                        if (chunk.fileName.includes('src/index.js')) {
+                            return `import "../style.css";`
+                        }
+                        return ''
+                    },
+                    preserveModules: true,
+                    dir: 'dist',
+                    entryFileNames: '[name].js',
+                    exports: 'named'
+                }
             }
-        }
-    },
-    plugins: [
-        tailwindcss(),
-        dts({ outDir: "./dist/types" }),
-        viteReact(),
-    ]
+        },
+        plugins: [
+            tailwindcss(),
+            dts({ outDir: "./dist/types" }),
+            viteReact(),
+        ]
+    }
 })
